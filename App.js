c14b913c9f79facb9669dfa7dda2965afed81bf7
Martin Van Aken <martin@joyouscoding.com>
2021-09-26 10:30:12 +0200

    Pagination working

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
 
 const URL = "https://opendata.bruxelles.be/api/records/1.0/search/?dataset=museums-in-brussels&q="
 
@@ -8,6 +8,10 @@ export default function App() {
   const [musees, setMusees] = useState([]);
   const [totalResults, setTotalResults] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
+
+  function buildPaginatedUrl(page) {
+    return URL + "&start=" + ((page-1) * 10)
+  }
 
   function renderMusee(item) {
     console.log(item)
@@ -19,16 +23,15 @@ export default function App() {
   }
 
   async function fetchData() {
-    let response = await fetch(URL);
+    let response = await fetch(buildPaginatedUrl(currentPage));
     let data = await response.json();
     setMusees(data.records)
     setTotalResults(data.nhits)
-    setCurrentPage(1)
   }
 
   useEffect(() => {
     fetchData();
-  }, [])
+  }, [currentPage])
 
   let totalPages = Math.ceil(totalResults / 10);
   
@@ -38,6 +41,8 @@ export default function App() {
         <Text>Vous visionnez la page {currentPage} sur {totalPages} disponibles ({totalResults} musées)</Text>
       </View>
         <FlatList data={musees} renderItem={renderMusee} keyExtractor={item => item.recordid} />
+        <Button disabled={currentPage === 1} title="Précédent" onPress={() => setCurrentPage(currentPage - 1)} />
+        <Button disabled={currentPage === totalPages} title="Suivant" onPress={() => setCurrentPage(currentPage + 1)} />
       <StatusBar style="auto" />
     </View>
   );
