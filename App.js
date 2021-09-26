fb74301d7a787e047d2b2d8ec3ea02dcf9b7ff5e
Martin Van Aken <martin@joyouscoding.com>
2021-09-26 10:19:31 +0200

    Add pagination info

--- App.js
+++ App.js
@@ -1,26 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 const URL = "https://opendata.bruxelles.be/api/records/1.0/search/?dataset=museums-in-brussels&q="
 
 export default function App() {
   const [musees, setMusees] = useState([]);
+  const [totalResults, setTotalResults] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  function renderMusee(item) {
+    console.log(item)
+    return (
+      <Text>
+        {item.item.fields.nom_du_musee}
+      </Text>
+    )
+  }
+
   async function fetchData() {
     let response = await fetch(URL);
     let data = await response.json();
     setMusees(data.records)
+    setTotalResults(data.nhits)
+    setCurrentPage(1)
   }
 
   useEffect(() => {
     fetchData();
   }, [])
+
+  let totalPages = Math.ceil(totalResults / 10);
   
   return (
     <View style={styles.container}>
-      { musees.map(musee => (
-        <Text key={musee.recordid}>{musee.fields.nom_du_musee}</Text>
-      ))}
+      <View>
+        <Text>Vous visionnez la page {currentPage} sur {totalPages} disponibles ({totalResults} musées)</Text>
+      </View>
+        <FlatList data={musees} renderItem={renderMusee} keyExtractor={item => item.recordid} />
       <StatusBar style="auto" />
     </View>
   );
